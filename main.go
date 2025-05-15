3807141a75ce6dc14036fe62170aa2a944230477
John Zetterman <jzetterman@outlook.com>
2025-05-15 15:05:47 -0400

    Added help and exit commands

--- main.go
+++ main.go
@@ -7,23 +7,49 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-
 	for {
 		fmt.Print("Pokedex > ")
+		scanner.Scan()
 
-		if !scanner.Scan() {
-			break
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
 		}
 
-		input := scanner.Text()
-		cleanInput := strings.ToLower(strings.Fields(input)[0])
-		fmt.Printf("Your command was: %s\n", cleanInput)
-	}
+		commandName := words[0]
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error: ", err)
+		if command, exists := getCommands()[commandName]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
 }
 
@@ -31,4 +57,23 @@ func cleanInput(text string) []string {
 	loweredText := strings.ToLower(text)
 	words := strings.Fields(loweredText)
 	return words
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, command := range getCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }
